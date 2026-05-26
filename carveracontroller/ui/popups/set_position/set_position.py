0415322789b3d3034afa1874d6187453714869e2
Software2 <software-2@users.noreply.github.com>
2026-05-25 18:15:14 -0700

    Refactor set_position popups to use generic bases

--- carveracontroller/ui/popups/set_position/set_position.py
+++ carveracontroller/ui/popups/set_position/set_position.py
@@ -5,9 +5,9 @@ from typing import Any
 
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.modalview import ModalView
 
-from carveracontroller.machine import coordinates
 from carveracontroller.translation import tr
 from carveracontroller.ui import widget_helpers
 
@@ -30,79 +30,31 @@ def _validate_float(text: str, label: str) -> tuple[bool, str]:
     return True, ""
 
 
-class SetXPopup(ModalView):
+class SingleFloatPopup(ModalView):
+    title = StringProperty("")
+    hint = StringProperty("")
+    label = StringProperty("")
+    machine_op = ObjectProperty(None)
+
     def __init__(self, coord_popup: Any, **kwargs: Any) -> None:
         self.coord_popup = coord_popup
         super().__init__(**kwargs)
 
-    def validate_inputs(self) -> tuple[bool, str]:
-        return _validate_float(self.ids.txt_offset.text, "X offset")
-
     def on_ok_pressed(self) -> None:
-        is_valid, error_message = self.validate_inputs()
+        is_valid, error = _validate_float(self.ids.txt_offset.text, self.label)
         if is_valid:
             app = App.get_running_app()
-            coordinates.set_x_offset(app.root.controller, float(self.ids.txt_offset.text))
+            self.machine_op(app.root.controller, float(self.ids.txt_offset.text))
             self.dismiss()
         else:
-            _show_error(error_message)
+            _show_error(error)
 
 
-class SetYPopup(ModalView):
-    def __init__(self, coord_popup: Any, **kwargs: Any) -> None:
-        self.coord_popup = coord_popup
-        super().__init__(**kwargs)
+class ToolPopup(ModalView):
+    title = StringProperty("")
+    hint = StringProperty("")
+    machine_op = ObjectProperty(None)
 
-    def validate_inputs(self) -> tuple[bool, str]:
-        return _validate_float(self.ids.txt_offset.text, "Y offset")
-
-    def on_ok_pressed(self) -> None:
-        is_valid, error_message = self.validate_inputs()
-        if is_valid:
-            app = App.get_running_app()
-            coordinates.set_y_offset(app.root.controller, float(self.ids.txt_offset.text))
-            self.dismiss()
-        else:
-            _show_error(error_message)
-
-
-class SetZPopup(ModalView):
-    def __init__(self, coord_popup: Any, **kwargs: Any) -> None:
-        self.coord_popup = coord_popup
-        super().__init__(**kwargs)
-
-    def validate_inputs(self) -> tuple[bool, str]:
-        return _validate_float(self.ids.txt_offset.text, "Z offset")
-
-    def on_ok_pressed(self) -> None:
-        is_valid, error_message = self.validate_inputs()
-        if is_valid:
-            app = App.get_running_app()
-            coordinates.set_z_offset(app.root.controller, float(self.ids.txt_offset.text))
-            self.dismiss()
-        else:
-            _show_error(error_message)
-
-
-class SetAPopup(ModalView):
-    def __init__(self, coord_popup: Any, **kwargs: Any) -> None:
-        self.coord_popup = coord_popup
-        super().__init__(**kwargs)
-
-    def validate_inputs(self) -> tuple[bool, str]:
-        return _validate_float(self.ids.txt_offset.text, "A offset")
-
-    def on_ok_pressed(self) -> None:
-        is_valid, error_message = self.validate_inputs()
-        if is_valid:
-            app = App.get_running_app()
-            coordinates.set_a_offset(app.root.controller, float(self.ids.txt_offset.text))
-            self.dismiss()
-        else:
-            _show_error(error_message)
-
-
-class SetToolPopup(ModalView):
     def __init__(self, coord_popup: Any, **kwargs: Any) -> None:
         self.coord_popup = coord_popup
         super().__init__(**kwargs)
@@ -113,38 +65,33 @@ class SetToolPopup(ModalView):
 
     def on_ok_pressed(self) -> None:
         app = App.get_running_app()
-        coordinates.set_tool_number(app.root.controller, self.ids.txt_offset.text)
+        self.machine_op(app.root.controller, self.ids.txt_offset.text)
         self.dismiss()
 
 
-class ChangeToolPopup(ModalView):
-    def __init__(self, coord_popup: Any, **kwargs: Any) -> None:
-        self.coord_popup = coord_popup
-        super().__init__(**kwargs)
-
-    def on_open(self) -> None:
-        super().on_open()
-        widget_helpers.bind_auto_select_to_text_input(self.txt_offset)
-
-    def on_ok_pressed(self) -> None:
-        app = App.get_running_app()
-        coordinates.change_tool(app.root.controller, self.ids.txt_offset.text)
-        self.dismiss()
+class SetXPopup(SingleFloatPopup):
+    pass
 
 
-class MoveAPopup(ModalView):
-    def __init__(self, coord_popup: Any, **kwargs: Any) -> None:
-        self.coord_popup = coord_popup
-        super().__init__(**kwargs)
+class SetYPopup(SingleFloatPopup):
+    pass
 
-    def validate_inputs(self) -> tuple[bool, str]:
-        return _validate_float(self.ids.txt_offset.text, "A position")
 
-    def on_ok_pressed(self) -> None:
-        is_valid, error_message = self.validate_inputs()
-        if is_valid:
-            app = App.get_running_app()
-            coordinates.rapid_move_a(app.root.controller, float(self.ids.txt_offset.text))
-            self.dismiss()
-        else:
-            _show_error(error_message)
+class SetZPopup(SingleFloatPopup):
+    pass
+
+
+class SetAPopup(SingleFloatPopup):
+    pass
+
+
+class MoveAPopup(SingleFloatPopup):
+    pass
+
+
+class SetToolPopup(ToolPopup):
+    pass
+
+
+class ChangeToolPopup(ToolPopup):
+    pass
