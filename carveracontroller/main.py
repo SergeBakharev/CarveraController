c3c327efe512989403065c129cec5670759342e5
Michael Ring <mail@michael-ring.org>
2026-07-23 13:07:21 +0200

    Received Measurements for the 4th Axis and added them

--- carveracontroller/main.py
+++ carveracontroller/main.py
@@ -4988,9 +4988,8 @@ class Makera(RelativeLayout):
             CNC.vars["rotation_base_width"] = 300
             CNC.vars["rotation_head_width"] = 56.5
         elif app.model == "Z1":
-            # TODO Add correct values for Z1
-            CNC.vars["rotation_base_width"] = 301
-            CNC.vars["rotation_head_width"] = 56
+            CNC.vars["rotation_base_width"] = 263
+            CNC.vars["rotation_head_width"] = 50
         elif app.model == "C1":
             if CNC.vars["FuncSetting"] & 1:
                 CNC.vars["rotation_base_width"] = 330
