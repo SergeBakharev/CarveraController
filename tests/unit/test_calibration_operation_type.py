fb71ec63daa3756879dc265da178cff02a629aca
Serge Bakharev <sergebakharev@users.noreply.github.com>
2026-07-24 19:02:40 +1000

    removing unnecessary test

--- tests/unit/test_calibration_operation_type.py
+++
@@ -1,9 +0,0 @@
-"""Tests for calibration operation labels."""
-
-from carveracontroller.addons.probing.operations.Calibration.CalibrationOperationType import (
-    CalibrationOperationType,
-)
-
-
-def test_anchor2_operation_uses_anchor2_title():
-    assert CalibrationOperationType.Anchor2.value.title == "Calibration - Anchor2"
